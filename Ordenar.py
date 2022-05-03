829c7b06b8a8bb7c7cc0c1ad39c833504357cd0b
Yan Gidorini <ygidorini@gmail.com>
2022-05-02 22:27:39 -0300

    Refactor
    refatorando o código.

--- Ordenar.py
+++ Ordenar.py
@@ -1,119 +1,110 @@
-from tkinter import E
+'''
+ALUNOS
+-------------------------
+| CHRISTOPHER (RM95083) |
+| EVELLY      (RM96090) |
+| VICTOR      (RM96141) |
+| YAN         (RM96190) |
+-------------------------
+'''
 
-
-email = "jose@gmail.com"
-
-# pontos:       6          5            4      3       2      diferença tempo (+recente = +pontos)
+# pontos:       60          50         40     30      20      diferença tempo (+recente = +pontos)
 # prioridade: senha > ajuda de senha > tel > nomes > email > data (mais recente para o mais antigo)
+
 empresas = {
     "000webhost": ["senha", "nome", "email", "03/2015"],
     "123RF": ["senha", "tel", "nome", "email", "03/2020"],
-    "500px": ["senha", "nome", "email", "06/2018"], 
+    "500px": ["senha", "nome", "email", "06/2018"],
     "8fit": ["senha", "nome", "email", "07/2018"],
-    "8tracks": ["senha", "email", "06/2017"]
-}
+    "8tracks": ["tel", "nome", "email", "06/2021"],
+    "devsfiap": ["nome", "email", "08/2016"],
+    "orocle": ["senha", "tel", "email", "05/2015"],
+    "semnai": ["tel", "nome", "03/2019"],
+    "newideas": ["senha","email", "05/2013"],
+    "animaljam": ["tel", "nome", "06/2017"],
+    "animoto": ["email", "tel", "nome", "senha", "03/2018"],
+    "bigbasket": ["senha", "email", "nome", "tel", "02/2021"],
+    "epik": ["email", "senha", "05/2013"],
+    "gett": ["senha", "email", "tel", "08/2017"],
+    "houzz": ["email", "09/2013"],
+    "pixlr": ["tel", "06/2009"],
+    "quidd": ["nome","email", "tel", "senha", "06/2021"],
+    "younoww": ["email", "tel", "03/2014"],
+    "zynga": ["tel", "05/2019"],
+    "yatra":["nome", "email", "06/2009"],
+ }
 
-pontosEmpresa = {} # cria dict vazio
+pontosEmpresa = {}  # cria dict vazio
 anoAtual = 2022
-    
+
 for empresa in empresas:
-    pontosEmpresa[empresa] = 0 # cria lista de pontos a partir da lista de empresas
-    
-    for i in range(len(empresas[empresa])): # percorre a list de cada empresa
-        match empresas[empresa][i]:
-            case "senha":
-                pontosEmpresa[empresa] += 6
-            case "tel":
-                pontosEmpresa[empresa] += 4
-            case "nome":
-                pontosEmpresa[empresa] += 3
-            case "email":
-                pontosEmpresa[empresa] += 2
+    # cria lista de pontos a partir da lista de empresas
+    pontosEmpresa[empresa] = 0
+
+    for i in range(len(empresas[empresa])):  # percorre a list de cada empresa
+        if empresas[empresa][i] == "senha":
+            pontosEmpresa[empresa] += 60
+
+        if empresas[empresa][i] == "tel":
+            pontosEmpresa[empresa] += 40
+
+        if empresas[empresa][i] == "nome":
+            pontosEmpresa[empresa] += 30
+
+        if empresas[empresa][i] == "email":
+            pontosEmpresa[empresa] += 20
     else:
         diff = anoAtual - int(empresas[empresa][i][3:7])
         decimal = int(empresas[empresa][i][0:2]) / 100
-        
-        match diff:
-            case 1:
-                pts = 10 + decimal
-                pontosEmpresa[empresa] += pts
-                round(pontosEmpresa[empresa], 2)
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 2:
-                pts = 9 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 3:
-                pts = 8 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 4:
-                pts = 7 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 5:
-                pts = 6 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 6:
-                pts = 5 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 7:
-                pts = 4 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 8:
-                pts = 3 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 9:
-                pts = 2 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
-            case 10:
-                pts = 1 + decimal
-                pontosEmpresa[empresa] += pts
-                pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)           
+
+        if diff == 1:
+            pts = 10 + decimal
+            pontosEmpresa[empresa] += pts
+            round(pontosEmpresa[empresa], 2)
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 2:
+            pts = 9 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 3:
+            pts = 8 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 4:
+            pts = 7 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 5:
+            pts = 6 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 6:
+            pts = 5 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 7:
+            pts = 4 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 8:
+            pts = 3 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 9:
+            pts = 2 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
+        elif diff == 10:
+            pts = 1 + decimal
+            pontosEmpresa[empresa] += pts
+            pontosEmpresa[empresa] = round(pontosEmpresa[empresa], 2)
 else:
-    
-    print(pontosEmpresa)
-    ordenado = sorted(pontosEmpresa.values(), reverse=True)
-    print(ordenado)
+    ordenado = sorted(pontosEmpresa.values(), reverse=True) #pegando os pontos e organizando eles de forma decrescente
+    resultado = {} #dicionário vazio onde será armazenado a empresa e seus pontos, de forma já ordenada
     
     for pontos in ordenado:
-        for empresa in pontosEmpresa:
-            if pontos == pontosEmpresa[empresa]:
-                print(empresa)
-    
-    
-
-
-# 2  10
-# 3  9
-# 4  8
-# 5  7
-# 6  6
-# 7  5
-# 8  4
-# 9  3
-# 10 2
-
-# pontosEmpresa = {
-#     "000webhost": 0,
-#     "123RF": 0,
-#     "500px": 0, 
-#     "8fit": 0,
-#     "8tracks": 0
-# }
-
-''' 
-como precisa ficar
-organizado = {
-    "123RF": ["senha", "tel", "nome", "email", "03/2020"] 24.03pts +,
-    "8fit": ["senha", "nome", "email", "07/2018"] 18.07pts +,
-    "500px": ["senha", "nome", "email", "06/2018"] 18.06pts +,
-    "000webhost": ["senha", "nome", "email", "03/2015"] 15.03tps +,
-    "8tracks": ["senha", "email", "06/2017"] 14.06pts +,
-}
-'''
+        for item in pontosEmpresa:
+            if pontos == pontosEmpresa[item]:
+                resultado[item] = pontos
+                
+                
